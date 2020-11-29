5244f7b304cd08e022242f6070a853da37456d66
Shawna Staff <staffs@my.lanecc.edu>
2020-11-28 18:12:26 -0800

    debugging exercise complete

---
+++ DebuggingPracticeB/Controllers/DemoController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HttpPractice.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PageTwo(string mascot)
+        {
+            ViewBag.Mascott = mascot;
+            return View();
+        }
+
+        public IActionResult PageThree()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz1()
+        {
+            Random rand = new Random();
+            ViewBag.Number1 = rand.Next(100);
+            ViewBag.Number2 = rand.Next(100);
+            return View();
+        }
+
+        public IActionResult Quiz1Answer(int number1, int number2, int answer)
+        {
+            string check = "wrong :-(";
+            if (number1 + number2 == answer)
+                check = "right!";
+            return Content(check);
+        }
+
+
+        [HttpGet]
+        public IActionResult Quiz2()
+        {
+            Random rand = new Random();
+            List<int> numbers = new List<int>();
+            numbers.Add(rand.Next(10));
+            numbers.Add(rand.Next(10));
+            return View(numbers);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz2(int number1, int number2, int answer)
+        {
+            string check = "wrong :-(";
+            if (number1 * number2 == answer)
+                check = "right!";
+            return Content(check);
+        }
+
+
+    }
+}
---
+++ DebuggingPracticeB/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult HomeTown(string state, string city)
+        {
+            string content = "State: " + state + ", City: " + city;
+            return Content(content);
+        }
+
+        [HttpPost]
+        public IActionResult FavoriteMusic(string genre, string author, string book)
+        {
+            return Content("Genre: " + genre + ", Author: " + book + ", Book: " + author);
+        }
+
+        [HttpPost]
+        public IActionResult Vacation(string location, string activity, string clothing)
+        {
+            string content = "Location: " + location + ", Activity: " + activity + ", What to wear: " + clothing;
+            return Content(content);
+        }
+
+        [HttpPost]
+        public IActionResult Reading(string genre, string author, string book)
+        {
+            string content = "Genre: " + genre + ", Author: " + author + ", Book: " + book;
+            return Content(content);
+        }
+
+    }
+
+}
